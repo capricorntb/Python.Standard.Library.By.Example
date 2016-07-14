77ed19ac123f4ff6f43973b0ecc873ba8542a0a0
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 16:12:09 +0800

    ooo000ooo
    Change-Id: Ib3006e535d59dcba6cc7c97b5a1531017281e4f8

--- re/re_flags_embedded.py
+++ re/re_flags_embedded.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Embedding flags in the expression.
 """
-#end_pymotw_header
 
 import re
 
--- re/re_groups_noncapturing.py
+++ re/re_groups_noncapturing.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Noncapturing groups
 """
-#end_pymotw_header
 
 from re_test_patterns_groups import test_patterns
 
