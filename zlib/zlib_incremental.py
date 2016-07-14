2b716b4405ae99da09d4f88cec736087cce186a5
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 16:27:16 +0800

    ooo000ooo
    Change-Id: I2f8a1df7c3fda27947d3e24843252fd3d9f23c7f

--- zlib/zlib_incremental.py
+++ zlib/zlib_incremental.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Incremental compression
 """
-#end_pymotw_header
 
 import zlib
 import binascii
