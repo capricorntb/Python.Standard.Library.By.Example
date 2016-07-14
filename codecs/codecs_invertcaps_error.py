13285c6150bdb5ea83a38dfe252d15cecb767486
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 14:40:25 +0800

    ooo000ooo
    Change-Id: I20ce45aed34e201b8bed4f2eb40bf93833bfec85

--- codecs/codecs_invertcaps_error.py
+++ codecs/codecs_invertcaps_error.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Error handling with character map encodings.
 """
-#end_pymotw_header
 
 import codecs
 from codecs_invertcaps_charmap import encoding_map
--- codecs/codecs_socket.py
+++ codecs/codecs_socket.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Sending Unicode data over a socket.
 """
-#end_pymotw_header
 
 import sys
 import SocketServer
