f38a1d3c3cc0348856c405f3adf0054c7f2ff420
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 09:48:11 +0800

    ooo000ooo
    Change-Id: If44ee8d8c28a04680e102ff85fd3fd427eaa8a13

--- string/string_constants.py
+++ string/string_constants.py
@@ -3,9 +3,11 @@
 
 import string
 
-for name in (s
-             for s in dir(string)
-             if not s.startswith('_')):
+for name in (
+                s
+                for s in dir(string)
+                if not s.startswith('_')
+            ):
     value = getattr(string, name)
     # Look for byte string and unicode values
     if isinstance(value, basestring):
