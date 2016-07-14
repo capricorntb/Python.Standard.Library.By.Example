d44fdd35fd28c461d74f59e9f8d59e62c2ab89a1
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 16:05:35 +0800

    ooo000ooo
    Change-Id: I9860c93b5f8e5f4a8d100e93f5277d46f9005fd2

--- re/re_email_verbose.py
+++ re/re_email_verbose.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Match email addresses
 """
-#end_pymotw_header
 
 import re
 
--- re/re_test_patterns_groups.py
+++ re/re_test_patterns_groups.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Show the groups within the matches for a pattern.
 """
-#end_pymotw_header
 
 import re
 
