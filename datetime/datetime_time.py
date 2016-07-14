12eb69fb0632b16497dc9ac9a5613eadaa4e71e4
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 17:34:41 +0800

    ooo000ooo
    Change-Id: I0c6a8fb0f61a11ae6b7c2b8fc5fba565f00ee432

--- datetime/datetime_time.py
+++ datetime/datetime_time.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2008 Doug Hellmann All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Basic features of time objects.
 """
-
-__version__ = "$Id$"
-#end_pymotw_header
 
 import datetime
 
