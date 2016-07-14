0d27fc09749dd19fa8c4150a5116d3f74a4aa249
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 14:02:12 +0800

    ooo000ooo
    Change-Id: I075e5b7d08aac8a10004b2f9645e8eab2841480b

--- select/select_echo_server_timeout.py
+++ select/select_echo_server_timeout.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Server half of echo example.
 """
-#end_pymotw_header
 
 import select
 import socket
