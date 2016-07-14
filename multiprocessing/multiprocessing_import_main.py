a7f50bb26293a4accacb6d12cef138c45547b05e
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 13:26:40 +0800

    ooo000ooo
    Change-Id: I5e1e3cca8599cbfb53a57a6910e0ff79878a9fbc

--- multiprocessing/multiprocessing_import_main.py
+++ multiprocessing/multiprocessing_import_main.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2008 Doug Hellmann All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Creating and waiting for a process
 """
-#end_pymotw_header
 
 import multiprocessing
 import multiprocessing_import_worker
