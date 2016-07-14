63c7d43fbe2a53a51d5a8aa655b990c0f7c23e1f
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 15:11:54 +0800

    ooo000ooo
    Change-Id: Iac96a82bc3718a03ea7c205b7c35ac086c13f8d1

--- functools/functools_cmp_to_key.py
+++ functools/functools_cmp_to_key.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Use total_ordering decorator to fill in missing rich comparison methods.
 """
-#end_pymotw_header
 
 import functools
 
