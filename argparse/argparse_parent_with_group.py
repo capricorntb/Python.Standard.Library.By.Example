362e67182c68a13db1fb8a1ad019cb3aadac6d84
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 17:30:58 +0800

    ooo000ooo
    Change-Id: I09460a0ca893edf4d349f40823d939644365b408

--- argparse/argparse_parent_with_group.py
+++ argparse/argparse_parent_with_group.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Parent with shared options
 """
-#end_pymotw_header
 
 import argparse
 
