24ab60a93e8bd0eea99f92a7abb243091c4aef25
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 16:52:37 +0800

    ooo000ooo
    Change-Id: If4621043e36dace042804ac85876fe68778118a1

--- inspect/inspect_getcallargs.py
+++ inspect/inspect_getcallargs.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Show how values map to function arguments.
 """
-#end_pymotw_header
 
 import inspect
 import example
