6b8741f5218fadf81ac2bc8e1a08e08a6676acb0
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 16:09:06 +0800

    ooo000ooo
    Change-Id: I15b647fadcdcd15395c468f7b17543c050fc3726

--- re/re_groups.py
+++ re/re_groups.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Regular expression grouping
 """
-#end_pymotw_header
 
 from re_test_patterns import test_patterns
 
