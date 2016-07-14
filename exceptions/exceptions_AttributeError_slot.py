5a9a9a4373c35e66b2bf5a2a0172ea48bd6b7ced
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 17:18:09 +0800

    ooo000ooo
    Change-Id: Id13d169fd2c5809d012314ce5334e9eecae83cc4

--- exceptions/exceptions_AttributeError_slot.py
+++ exceptions/exceptions_AttributeError_slot.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """AttributeError from class that uses __slots__
 """
-#end_pymotw_header
 
 class MyClass(object):
     __slots__ = ( 'attribute', )
