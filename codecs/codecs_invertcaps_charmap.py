4a243f2daa06c00b7ce66d664e32ea0f70a31b03
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 14:41:28 +0800

    ooo000ooo
    Change-Id: I91b546f2b314807662bde34756cc89b374a05f9d

--- codecs/codecs_invertcaps_charmap.py
+++ codecs/codecs_invertcaps_charmap.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Character mapping encoder
 """
-#end_pymotw_header
 
 import codecs
 import string
