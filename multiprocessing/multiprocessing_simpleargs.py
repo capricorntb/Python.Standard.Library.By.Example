116a2f6269340421c6a55998d32337bf9b98a253
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 13:22:25 +0800

    ooo000ooo
    Change-Id: I6b5176ec284312dcea58e977a212e58b7982e9c1

--- multiprocessing/multiprocessing_simpleargs.py
+++ multiprocessing/multiprocessing_simpleargs.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2008 Doug Hellmann All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Passing arguments to threads when they are created
 """
-#end_pymotw_header
 
 import multiprocessing
 
