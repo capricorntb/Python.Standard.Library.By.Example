21213bb15a2688e468fd6ec2beff4a1d2103e081
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 17:20:27 +0800

    ooo000ooo
    Change-Id: Id4977f7dba3dff004136e4c5226b8a9045e3b126

--- exceptions/exceptions_ImportError_nomodule.py
+++ exceptions/exceptions_ImportError_nomodule.py
@@ -1,12 +1,4 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2008 Doug Hellmann All rights reserved.
-#
-"""
-"""
-
-__version__ = "$Id$"
-#end_pymotw_header
+# -*- coding: utf-8 -*-
 
 import module_does_not_exist
