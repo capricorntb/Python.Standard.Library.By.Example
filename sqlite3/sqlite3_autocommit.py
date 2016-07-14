b1128c3fe48a22f67131a6c98d966ea2b8a1fdf2
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 17:41:40 +0800

    ooo000ooo
    Change-Id: I0460c7711a12e9e182f55f641a64a889a933a7f0

--- sqlite3/sqlite3_autocommit.py
+++ sqlite3/sqlite3_autocommit.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Illustrate the effect of autocommit mode.
 """
-#end_pymotw_header
 
 import logging
 import sqlite3
