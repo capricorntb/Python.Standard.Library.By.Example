f8217ca1af97cdd5f1246aa6fa93eaed125e5812
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 15:16:28 +0800

    ooo000ooo
    Change-Id: I9fa5a941406ed8e649fb6cb292e6e8ad205a31c3

--- cgitb/cgitb_log_exception.py
+++ cgitb/cgitb_log_exception.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2009 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Logging exceptions to a file, instead of displaying them.
 """
-#end_pymotw_header
 
 import cgitb
 import os
