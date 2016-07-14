02727d84fa8d706d1135f5bf08b5e25ce87808b9
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 17:29:24 +0800

    ooo000ooo
    Change-Id: I48f603ee114ef85985bef32abbde8dbfe5203e33

--- argparse/argparse_action.py
+++ argparse/argparse_action.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Show the built-in argument actions.
 """
-#end_pymotw_header
 
 import argparse
 
