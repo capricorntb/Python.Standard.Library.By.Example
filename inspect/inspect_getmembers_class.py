a45505e542e2a38d03efd03f933a8db7db1b6006
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 19:01:21 +0800

    ooo000ooo
    Change-Id: I3b551cbb63b97426950661b6ebf91bae07554996

--- inspect/inspect_getmembers_class.py
+++ inspect/inspect_getmembers_class.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 """Using getmembers()
 """
-#end_pymotw_header
 
 import inspect
 from pprint import pprint
--- inspect/inspect_stack.py
+++ inspect/inspect_stack.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 """Inspecting the call stack.
 """
-#end_pymotw_header
 
 import inspect
 
