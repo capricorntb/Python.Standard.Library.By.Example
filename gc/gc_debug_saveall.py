a7dbc2d3e2592c44f4fda733584c7a60dc964462
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 15:00:04 +0800

    ooo000ooo
    Change-Id: Iee5d0f5dc0e5a03401986b384126e7cb6b4e92b3

--- gc/gc_debug_saveall.py
+++ gc/gc_debug_saveall.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Show the objects with references to a given object.
 """
-#end_pymotw_header
 
 import gc
 
--- gc/gc_get_objects.py
+++ gc/gc_get_objects.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Examine the objects being managed.
 """
-#end_pymotw_header
 
 import gc
 
