7dc257005db8a5fc9cfcdddd91d6786062ee87b6
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 11:00:05 +0800

    ooo000ooo
    Change-Id: I46060f0a74d3ac85ca76e41f96a82215c3ad4666

--- math/math_expm1.py
+++ math/math_expm1.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Logarithms close to zero.
 """
-#end_pymotw_header
 
 import math
 
