6b588c54720e688496b0722c59931ef675f47ae8
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 16:11:21 +0800

    ooo000ooo
    Change-Id: Ic9242b8949b9b16bb61c1fea8cdb38fa01a3a567

--- re/re_refer_to_group.py
+++ re/re_refer_to_group.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Find email addresses that match the person's name
 """
-#end_pymotw_header
 
 import re
 
--- re/re_simple.py
+++ re/re_simple.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Simple pattern examples.
 """
-#end_pymotw_header
 
 import re
 
