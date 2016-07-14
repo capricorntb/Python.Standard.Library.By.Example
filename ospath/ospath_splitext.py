ea54e946c2beebdd04282cba43020dd81e8bee52
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 11:17:25 +0800

    ooo000ooo
    Change-Id: I5c808fc165e12d45c4e804ea27147b25f94eebde

--- ospath/ospath_splitext.py
+++ ospath/ospath_splitext.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2008 Doug Hellmann All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Separate a filename into the base and extension.
 """
 
 __version__ = "$Id$"
-#end_pymotw_header
 
 import os.path
 
