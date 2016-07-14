c95aae59e3cd99f8b399139606372b1258836411
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 17:37:41 +0800

    ooo000ooo
    Change-Id: I8346fbd41353b8ae7275ed0cabe67c373e08316c

--- datetime/datetime_datetime.py
+++ datetime/datetime_datetime.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2008 Doug Hellmann All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Combining dates and times into a single object.
 """
-
-__version__ = "$Id$"
-#end_pymotw_header
 
 import datetime
 
