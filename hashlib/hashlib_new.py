b98d02eb4e2c12c2becef22bf0beb2b08a8cbfea
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 18:53:04 +0800

    ooo000ooo
    Change-Id: Ia8801b5d9c4ab994402f093cc0aa72c32665d9d5

--- hashlib/hashlib_new.py
+++ hashlib/hashlib_new.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2008 Doug Hellmann All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Naming the hash type using a string.
 """
-
-__version__ = "$Id$"
-#end_pymotw_header
 
 import hashlib
 import sys
--- hashlib/hashlib_update.py
+++ hashlib/hashlib_update.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2008 Doug Hellmann All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Calling update() more than once is like calling it with all of the data at once.
 """
-
-__version__ = "$Id$"
-#end_pymotw_header
 
 import hashlib
 
