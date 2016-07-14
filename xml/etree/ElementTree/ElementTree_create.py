4693de05354fe056a33d4119181c4c0a19923c30
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 17:01:19 +0800

    ooo000ooo
    Change-Id: I001fb6c47605ed61f22c00aa79f1e620e6dfe1f6

--- xml/etree/ElementTree/ElementTree_create.py
+++ xml/etree/ElementTree/ElementTree_create.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Creating XML documents
 """
-#end_pymotw_header
 
 from xml.etree.ElementTree import ( Element,
                                     SubElement,
--- xml/etree/ElementTree/ElementTree_extend_node.py
+++ xml/etree/ElementTree/ElementTree_extend_node.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Creating XML documents with lists of nodes
 """
-#end_pymotw_header
 
 from xml.etree.ElementTree import Element, SubElement, tostring, XML
 from ElementTree_pretty import prettify
