f4d1a735c04cd9ba0490e1a5e59ca2a705f2b526
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 17:21:11 +0800

    ooo000ooo
    Change-Id: I565244a7e9320ce64e22967340d6434736c446ef

--- exceptions/exceptions_IndexError.py
+++ exceptions/exceptions_IndexError.py
@@ -1,13 +1,5 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2008 Doug Hellmann All rights reserved.
-#
-"""
-"""
-
-__version__ = "$Id$"
-#end_pymotw_header
+# -*- coding: utf-8 -*-
 
 my_seq = [ 0, 1, 2 ]
 print my_seq[3]
--- exceptions/exceptions_TypeError.py
+++ exceptions/exceptions_TypeError.py
@@ -1,12 +1,4 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2008 Doug Hellmann All rights reserved.
-#
-"""
-"""
-
-__version__ = "$Id$"
-#end_pymotw_header
+# -*- coding: utf-8 -*-
 
 result = 5 + 'string'
