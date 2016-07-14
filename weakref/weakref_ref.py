20ff358b8e39d7a1884d0b2f5094d01765a1e61f
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 16:20:46 +0800

    ooo000ooo
    Change-Id: I02a0b34a8acac30cf3e2960ec4e1338e266ce334

--- weakref/weakref_ref.py
+++ weakref/weakref_ref.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2008 Doug Hellmann All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Example using weakref.ref to manage a reference to an object.
 """
-#end_pymotw_header
 
 import weakref
 
