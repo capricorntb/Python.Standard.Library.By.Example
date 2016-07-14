9039c24c8a85df0ce1dee5da3c012874b7f7a886
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 14:30:31 +0800

    ooo000ooo
    Change-Id: Id2832a1950ab42185ae6f152edfa5828422418e9

--- ConfigParser/ConfigParser_allow_no_value.py
+++ ConfigParser/ConfigParser_allow_no_value.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Reading a configuration file.
 """
-#end_pymotw_header
 
 import ConfigParser
 
