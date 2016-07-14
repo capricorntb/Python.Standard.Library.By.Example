37c6bf5daa710a2c9032a11697cf2951b4e6512b
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 16:10:26 +0800

    ooo000ooo
    Change-Id: I088235604125a360c2f5bcb9465d8ecb45e21d23

--- re/re_groups_match.py
+++ re/re_groups_match.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Looking at groups on a match object
 """
-#end_pymotw_header
 
 import re
 
