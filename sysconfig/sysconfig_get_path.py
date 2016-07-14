c3cc63dc222b42317ff66be739eea36281009a92
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 13:38:39 +0800

    ooo000ooo
    Change-Id: I00d61109eb22324c865234ef7b28bb5c40611319

--- sysconfig/sysconfig_get_path.py
+++ sysconfig/sysconfig_get_path.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """The paths for a scheme.
 """
-#end_pymotw_header
 
 import sysconfig
 import pprint
--- sysconfig/sysconfig_get_path_names.py
+++ sysconfig/sysconfig_get_path_names.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """The names of the paths in a scheme.
 """
-#end_pymotw_header
 
 import sysconfig
 
