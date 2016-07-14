88aae07809156f2a56d2f02a76b6082fe05b6ab3
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 17:41:06 +0800

    ooo000ooo
    Change-Id: I36cb06b4ef2caa8d8cb46640bd8a8be97800fc6b

--- sqlite3/sqlite3_custom_type.py
+++ sqlite3/sqlite3_custom_type.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Defining a custom type.
 """
-#end_pymotw_header
 
 import sqlite3
 try:
