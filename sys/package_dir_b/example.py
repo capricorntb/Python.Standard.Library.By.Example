bc7a4825f1db291b1a87ddd31294167218eece11
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 13:48:51 +0800

    ooo000ooo
    Change-Id: Id2dd0ecc873f9e9e4ca8aef8452ad04817a1c832

--- sys/package_dir_b/example.py
+++ sys/package_dir_b/example.py
@@ -1,10 +1,4 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2009 Doug Hellmann All rights reserved.
-#
-"""
-"""
-#end_pymotw_header
+# -*- coding: utf-8 -*-
 
 DATA = 'This is example B'
