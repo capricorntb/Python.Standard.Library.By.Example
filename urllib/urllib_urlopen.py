538623d3aab74fee35193dd5b8bd7112790a894e
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 16:01:44 +0800

    ooo000ooo
    Change-Id: I49b67573052214d0d7f4ec09bf2363e14b6368c2

--- urllib/urllib_urlopen.py
+++ urllib/urllib_urlopen.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2008 Doug Hellmann All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Simple example with urllib.urlopen().
 """
-#end_pymotw_header
 
 import urllib
 
