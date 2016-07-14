508b47aa303ec13de37478eb2f1b7c546be9b09f
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 13:38:01 +0800

    ooo000ooo
    Change-Id: I39beeb82fe39719967220a260db3622dd30cf010

--- sysconfig/sysconfig_get_platform.py
+++ sysconfig/sysconfig_get_platform.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Platform specifier for binary modules
 """
-#end_pymotw_header
 
 import sysconfig
 
