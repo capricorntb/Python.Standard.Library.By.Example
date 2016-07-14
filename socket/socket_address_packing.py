7b86517cf9fbea7cbd0e7228f067bf560d9cd64c
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 17:45:52 +0800

    ooo000ooo
    Change-Id: Ifc58ff34e0710df396d6709b3f0c5dcfcc184581

--- socket/socket_address_packing.py
+++ socket/socket_address_packing.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Converting between string and binary representations of addresses.
 """
-#end_pymotw_header
 
 import binascii
 import socket
