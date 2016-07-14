9346b5ff0ca2969f56d0512530f0b4afb66450ed
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 14:49:47 +0800

    ooo000ooo
    Change-Id: Id980998d8d8314812aaa028dcbb0ae0e22eb81e7

--- gettext/gettext_module_global.py
+++ gettext/gettext_module_global.py
@@ -1,11 +1,5 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2009 Doug Hellmann All rights reserved.
-#
-"""
-"""
-#end_pymotw_header
+# -*- coding: utf-8 -*-
 
 import gettext
 t = gettext.translation('gettext_example', 'locale', fallback=True)
