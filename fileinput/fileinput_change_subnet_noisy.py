c53370f1e9e87f8a8044a6745c70243f7a435617
Adil.Zhu <Adil.Zhu@Qisda.com>
2016-07-14 15:14:10 +0800

    ooo000ooo
    Change-Id: Ic2b5566955ae0e4a4f448c9c5c24b2f5b3a7e91e

--- fileinput/fileinput_change_subnet_noisy.py
+++ fileinput/fileinput_change_subnet_noisy.py
@@ -1,11 +1,8 @@
 #!/usr/bin/env python
-# encoding: utf-8
-#
-# Copyright (c) 2010 Doug Hellmann.  All rights reserved.
-#
+# -*- coding: utf-8 -*-
+
 """Simplistic grep implementation
 """
-#end_pymotw_header
 
 import fileinput
 import glob
